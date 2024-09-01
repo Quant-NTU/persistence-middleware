322d4ed60ff2dd2ef7a8edfec2ffd6b260b0548e
Ivsucram <ivsucram@gmail.com>
2024-09-02 04:34:31 +0800

    Refactor and standardize docker-compose files

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,25 @@
 ARG GRADLE_VERSION=latest
 ARG OPENJDK_VERSION=21-slim
 
+FROM gradle:${GRADLE_VERSION} AS dev
+WORKDIR /src
+
 FROM gradle:${GRADLE_VERSION} AS gradle
-COPY --chown=gradle:gradle . /app/persistence-middleware
-WORKDIR /app/persistence-middleware
+WORKDIR /src
+COPY --chown=gradle:gradle . /src
 RUN gradle build -x test
 
-FROM gradle:${GRADLE_VERSION} AS dev
-
 FROM openjdk:${OPENJDK_VERSION} AS jdk
+COPY --from=gradle /src/build/libs/quant-ai-persistence-middleware.jar /src/server/server.jar
 
 FROM jdk AS local
-COPY --from=gradle /app/persistence-middleware/build/libs/*.jar /app/server.jar
 EXPOSE 8080
-ENTRYPOINT ["java", "-Dspring.profiles.active=local", "-jar", "/app/server.jar"]
+ENTRYPOINT ["java", "-Dspring.profiles.active=local", "-jar", "/src/server/server.jar"]
 
 FROM jdk AS stag
-COPY --from=gradle /app/persistence-middleware/build/libs/*.jar /app/server.jar
 EXPOSE 8080
-ENTRYPOINT ["java", "-Dspring.profiles.active=stag", "-jar", "/app/server.jar"]
+ENTRYPOINT ["java", "-Dspring.profiles.active=stag", "-jar", "/src/server/server.jar"]
 
 FROM jdk AS prod
-COPY --from=gradle /app/persistence-middleware/build/libs/*.jar /app/server.jar
 EXPOSE 8080
-ENTRYPOINT ["java", "-Dspring.profiles.active=prod", "-jar", "/app/server.jar"]
+ENTRYPOINT ["java", "-Dspring.profiles.active=prod", "-jar", "/src/server/server.jar"]
---
+++ dev-setup.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+usage() {
+    echo "Usage: $0 [OPTIONS]"
+    echo "Options:"
+    echo "-h, --help                    Display this help message"
+    echo "-b, --build                   Compile and Build the current gradle project"
+    echo "-t, --test                    Compile, Build and Test the current gradle project"
+    echo "-l, --local                   Compile, Build and Serve using local configs"
+    echo "-s, --stag,  --staging        Compile, Build and Serve using stating configs"
+    echo "-p, --prod,  --production     Compile, Build and Serve using prodution configs"
+}
+
+handle_options() {
+    while [ $# -gt 0 ]; do
+        case $1 in
+            -h | --help)
+                usage
+                exit 0
+            ;;
+
+            -b | --build)
+                gradle build -x test
+            ;;
+
+            -t | --test)
+                gradle test
+                gradle build
+            ;;
+
+            -l | --local)
+                gradle build -x test
+                java -Dspring.profiles.active=local -jar /src/build/libs/quant-ai-persistence-middleware.jar
+            ;;
+
+            -s | --stag | --staging)
+                gradle build -x test
+                java -Dspring.profiles.active=stag -jar /src/build/libs/quant-ai-persistence-middleware.jar
+            ;;
+
+            -p | --prod | --production)
+                gradle build -x test
+                java -Dspring.profiles.active=prod -jar /src/build/libs/quant-ai-persistence-middleware.jar
+            ;;
+
+            *)
+                echo "Invalid option: $1" >&2
+                usage
+                exit 1
+            ;;
+        esac
+        shift
+    done
+}
+
+handle_options "$@"
