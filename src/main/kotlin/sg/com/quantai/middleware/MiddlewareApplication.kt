2b63837c24d850998d4cc003c6c820d29d3b6097
Ivsucram <ivsucram@gmail.com>
2024-10-16 00:48:12 +0800

    Fix swagger build (but swagger is not accessible yet)

--- src/main/kotlin/sg/com/quantai/middleware/MiddlewareApplication.kt
+++ src/main/kotlin/sg/com/quantai/middleware/MiddlewareApplication.kt
@@ -2,14 +2,6 @@ package sg.com.quantai.middleware
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-// Added for swagger
-import org.springframework.web.servlet.config.annotation.EnableWebMvc; 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-
-//Added for swagger #
-@EnableSwagger2
-@EnableWebMvc
 
 @SpringBootApplication
 class MiddlewareApplication
