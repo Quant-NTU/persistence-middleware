2e8b1f2055ee207faf876ff4101f72bfd00e7b04
Abhi <abhishekhpandey2000@gmail.com>
2024-12-20 12:15:13 +0800

    Asset classes working

--- src/main/kotlin/sg/com/quantai/middleware/data/Asset.kt
+++ src/main/kotlin/sg/com/quantai/middleware/data/Asset.kt
@@ -2,17 +2,13 @@ package sg.com.quantai.middleware.data
 
 import java.time.LocalDateTime
 import org.bson.types.ObjectId
-import org.springframework.data.annotation.Id
-import org.springframework.data.mongodb.core.index.Indexed
 import java.math.BigDecimal
 
 open class Asset(
-    @Id val _id: ObjectId = ObjectId.get(), // document id, it changes when updated via upsert
-    @Indexed(unique = true) val uid: String = ObjectId.get().toString(),
-    val name: String,
-    val symbol: String,
-    val quantity: BigDecimal,
-    val purchasePrice: BigDecimal,
+    open val name: String,
+    open val symbol: String,
+    open val quantity: BigDecimal,
+    open val purchasePrice: BigDecimal,
     val createdDate: LocalDateTime = LocalDateTime.now(),
     val updatedDate: LocalDateTime = LocalDateTime.now(),
 )
