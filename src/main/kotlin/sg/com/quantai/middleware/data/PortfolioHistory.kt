60f80457b13a111630750a67483590ef81afcb4c
Abhi <abhishekhpandey2000@gmail.com>
2025-02-06 02:35:08 +0800

    Created NewPortfolio, PortfolioHistory and NewPortfolioController

---
+++ src/main/kotlin/sg/com/quantai/middleware/data/PortfolioHistory.kt
@@ -0,0 +1,25 @@
+package sg.com.quantai.middleware.data
+
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
+import java.math.BigDecimal
+import org.springframework.data.mongodb.core.mapping.DBRef
+import sg.com.quantai.middleware.data.Asset
+import sg.com.quantai.middleware.data.NewPortfolio 
+
+enum class PortfolioAction { BUY, SELL, ADD, REMOVE }
+
+data class NewPortfolioHistory(
+    @Id
+    val id: ObjectId = ObjectId.get(),
+    val asset: Asset,                   //not sure if this is correct but based on theory I think it is                   
+    val action: PortfolioAction,
+    val quantity: BigDecimal,
+    val value: BigDecimal,
+
+    @DBRef
+    val owner: NewPortfolio, //To map portfoliohistory to new portfolio 
+    
+    @Id
+    val _id: ObjectId = ObjectId.get()
+)
