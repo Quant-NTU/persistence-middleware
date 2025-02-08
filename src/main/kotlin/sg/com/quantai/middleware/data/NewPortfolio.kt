3949da65e9556cc20445428c6ef9d6e840af9b7b
Zoey Lam <92968261+Zomankey@users.noreply.github.com>
2025-02-09 00:28:02 +0800

    data files

---
+++ src/main/kotlin/sg/com/quantai/middleware/data/NewPortfolio.kt
@@ -0,0 +1,22 @@
+package sg.com.quantai.middleware.data
+
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.Indexed
+import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.core.mapping.DBRef
+import sg.com.quantai.middleware.data.PortfolioHistory  
+import sg.com.quantai.middleware.data.User 
+
+@Document(collection = "newPortfolios")
+data class NewPortfolio(
+    @Id val id: ObjectId = ObjectId.get(),
+    val isMain: Boolean,
+    
+    @DBRef val owner: User,
+    @DBRef(lazy=true) val history: List<PortfolioHistory>? = emptyList<PortfolioHistory>(),
+    @DBRef(lazy=true) val assets: List<Asset>? = emptyList<Asset>(),
+
+    @Indexed(unique = true)
+    val uid: String = ObjectId.get().toString() //assigns uid that mongo uses to identify the object
+)
