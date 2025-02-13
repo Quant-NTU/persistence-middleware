c454b93262824c0fe5f379bf9f993d4d8b624d6a
ivs <ivsucram@gmail.com>
2025-02-13 12:00:57 +0800

    Remove unused import

--- src/main/kotlin/sg/com/quantai/middleware/MiddlewareApplication.kt
+++ src/main/kotlin/sg/com/quantai/middleware/MiddlewareApplication.kt
@@ -2,7 +2,6 @@ package sg.com.quantai.middleware
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
