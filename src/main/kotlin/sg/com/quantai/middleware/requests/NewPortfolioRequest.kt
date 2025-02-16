370b4f78c1e4f94dea4fc7ba8d116dfa29f4a6f9
Zoey Lam <92968261+Zomankey@users.noreply.github.com>
2025-02-16 20:54:29 +0800

    minor changes

--- src/main/kotlin/sg/com/quantai/middleware/requests/NewPortfolioRequest.kt
+++ src/main/kotlin/sg/com/quantai/middleware/requests/NewPortfolioRequest.kt
@@ -3,8 +3,7 @@ import java.math.BigDecimal
 
 
 class NewPortfolioRequest(
-        val isMain: Boolean,
-        val assets: String,
+        val isMain: String,
         val description: String,
         val name: String,
 )
