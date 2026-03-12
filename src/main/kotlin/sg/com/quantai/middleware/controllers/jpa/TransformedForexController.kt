189696bdd044b4059068ad90e74f0a052ec3db1a
Geng Yang <CGY1539@users.noreply.github.com>
2026-03-12 21:03:26 +0800

    made regex a companion object

--- src/main/kotlin/sg/com/quantai/middleware/controllers/jpa/TransformedForexController.kt
+++ src/main/kotlin/sg/com/quantai/middleware/controllers/jpa/TransformedForexController.kt
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/forex/transformed")
 class TransformedForexController(private val service: TransformedForexService) {
 
+    companion object {
+        private val CURRENCY_PAIR_REGEX = Regex("^[A-Z]{3}\\|[A-Z]{3}$")
+    }
+
     @GetMapping
     fun getAllTransformedData(
         @RequestParam(defaultValue = "100") limit: Int
@@ -27,7 +31,7 @@ class TransformedForexController(private val service: TransformedForexService) {
     ): ResponseEntity<List<TransformedForex>> {
         // Original pairs come in format XXX/YYY but '/' splits the path, could not find a way to escape the '/' in the uri
         // Resorted to changing XXX/YYY to XXX|YYY when building the uri, then changing it back to XXX/YYY (how it is named in DB)
-        if (!Regex("^[A-Z]{3}\\|[A-Z]{3}$").matches(currencyPair)) {
+        if (!CURRENCY_PAIR_REGEX.matches(currencyPair)) {
             return ResponseEntity.badRequest().build()
         }
 
